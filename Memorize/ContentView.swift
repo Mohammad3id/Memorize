4dc6e3fa4e52a6618238ceff3ddae40ec5616f29
Mohammad Eid <mohammad.mossad.eid@gmail.com>
2025-01-22 02:22:44 +0200

    improved assignment implementation

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,24 +7,45 @@
 
 import SwiftUI
 
+enum CardTheme {
+    case halloween, nature, animals
+    
+    var color: Color {
+        switch self {
+        case .halloween: .orange
+        case .nature: .green
+        case .animals: .brown
+        }
+    }
+    
+    var emojis: [String] {
+        switch self {
+        case .halloween: ["🎃", "👻", "🕷️", "😈", "💀", "🧙", "🙀", "👹", "😱", "☠️"]
+        case .nature: ["🌲", "🌻", "🌊", "☀️", "🌸", "🍂", "⛰️", "🌵", "🍃", "🌈"]
+        case .animals: ["🐶", "🐱", "🦁", "🐵", "🐸", "🦉", "🐢", "🐰", "🦊", "🐼"]
+        }
+    }
+    
+    var symbol: String {
+        switch self {
+        case .halloween: "ant"
+        case .nature: "leaf"
+        case .animals: "pawprint"
+        }
+    }
+    
+    var label: String {
+        switch self {
+        case .halloween: "Halloween"
+        case .nature: "Nature"
+        case .animals: "Animals"
+        }
+    }
+}
 
 struct ContentView: View {
-    let halloweenEmojis = ["🎃", "👻", "🕷️", "😈", "💀", "🧙", "🙀", "👹", "😱", "☠️"]
-    let natureEmojis = ["🌲", "🌻", "🌊", "☀️", "🌸", "🍂", "⛰️", "🌵", "🍃", "🌈"]
-    let animalEmojis = ["🐶", "🐱", "🦁", "🐵", "🐸", "🦉", "🐢", "🐰", "🦊", "🐼"]
-    
-    let halloweenColor = Color.orange
-    let natureColor = Color.green
-    let animalColor = Color.brown
-    
-    @State var selectedThemeEmojis: [String]
-    @State var selectedThemeColor: Color
-    
-    init() {
-        let emojis = halloweenEmojis.shuffled().dropFirst((1...3).randomElement()! * 2)
-        selectedThemeEmojis = (emojis + emojis).shuffled()
-        selectedThemeColor = halloweenColor
-    }
+    @State var cardEmojis = [String]()
+    @State var selectedTheme = CardTheme.halloween
     
     var body: some View {
         VStack {
@@ -37,39 +58,46 @@ struct ContentView: View {
             themeButtons
         }
         .padding()
+        .onAppear {
+            cardEmojis = generatePairEmojis(from: selectedTheme)
+        }
     }
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
-            ForEach(selectedThemeEmojis.indices, id: \.self) { i in
-                CardView(content: selectedThemeEmojis[i])
+            ForEach(cardEmojis.indices, id: \.self) { i in
+                CardView(content: cardEmojis[i])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
-        .foregroundStyle(selectedThemeColor)
+        .foregroundStyle(selectedTheme.color)
     }
     
     var themeButtons: some View {
         HStack {
-            themeButton(for: "Halloween", symbol: "ant", emojis: halloweenEmojis, color: halloweenColor)
+            themeButton(.halloween)
             Spacer()
-            themeButton(for: "Nature", symbol: "leaf", emojis: natureEmojis, color: natureColor)
+            themeButton(.nature)
             Spacer()
-            themeButton(for: "Animals", symbol: "pawprint", emojis: animalEmojis, color: animalColor)
+            themeButton(.animals)
         }
     }
     
-    func themeButton(for label: String, symbol: String, emojis: [String], color: Color) -> some View {
+    func themeButton(_ theme: CardTheme) -> some View {
         Button {
-            let emojis = emojis.shuffled().dropFirst((1...3).randomElement()! * 2)
-            selectedThemeEmojis = (emojis + emojis).shuffled()
-            selectedThemeColor = color
+            selectedTheme = theme
+            cardEmojis = generatePairEmojis(from: theme)
         } label: {
             VStack {
-                Image(systemName: symbol)
-                Text(label)
+                Image(systemName: theme.symbol)
+                Text(theme.label)
             }
         }
+    }
+    
+    func generatePairEmojis(from theme: CardTheme) -> [String] {
+        let pairEmojis = theme.emojis.shuffled().dropFirst((1...3).randomElement()! * 2)
+        return (pairEmojis + pairEmojis).shuffled()
     }
 }
 
@@ -80,7 +108,6 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let rectangle = RoundedRectangle(cornerRadius: 12, style: .circular)
-            
             
             rectangle.foregroundStyle(.white)
             rectangle.stroke(lineWidth: 2)
